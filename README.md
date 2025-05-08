930ff15f6faf9324acccff466968db03640f7ad9
yctsai <yctsai@itri.org.tw>
2025-05-08 23:13:24 +0800

    建資料表，增加筆記本

--- README.md
+++ README.md
@@ -8,3 +8,32 @@ Step3 儲存並推送修改：
 git add .
 git commit -m "說明這次修改了什麼（例如：完成首頁排版）"
 git push# ticket-system
+
+## Git
+所有操作在終端機執行
+### 第一次拉資料夾
+```
+git clone git@github.com:Aurororara/ticket-system.git
+```
+### 每次編輯時更新 git
+```
+git pull origin main
+```
+### 上 git
+`git add .` -> 代表所有檔案都要上傳上去
+```
+git add .
+git commit -m "說明這次修改了什麼（例如：完成首頁排版）"
+git push origin main
+```
+
+## Python
+### 後端 python 安裝套件
+```
+pip install -r requirements.txt
+```
+
+### 執行服務
+```
+python app.py
+```
--- app.py
+++ app.py
@@ -1,31 +1,43 @@
-from django import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request
+from config import Config
+from models import db
+from models.models import Show, Host, Location, Game, Area, GameArea, Member, Order, Ticket, Payment
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+# 初始化資料庫
+db.init_app(app)
+# 建立資料表
+with app.app_context():
+  db.create_all()
+  print("資料表已建立完成")
+
 
 # 模擬票券資料（你也可以接資料庫）
 tickets = [
-    {"id": 1, "title": "ENHYPEN 演唱會", "stock": 10},
-    {"id": 2, "title": "IU 台北場", "stock": 5}
+  {"id": 1, "title": "ENHYPEN 演唱會", "stock": 10},
+  {"id": 2, "title": "IU 台北場", "stock": 5}
 ]
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
 
 @app.route('/api/tickets')
 def get_tickets():
-    return jsonify({"tickets": tickets})
+  return jsonify({"tickets": tickets})
 
 @app.route('/api/buy', methods=['POST'])
 def buy_ticket():
-    data = request.get_json()
-    ticket_id = data.get('ticket_id')
+  data = request.get_json()
+  ticket_id = data.get('ticket_id')
 
-    for ticket in tickets:
-        if ticket['id'] == ticket_id and ticket['stock'] > 0:
-            ticket['stock'] -= 1
-            return jsonify({"message": "搶票成功！剩餘：" + str(ticket['stock'])})
-    return jsonify({"message": "搶票失敗，可能已售完"}), 400
+  for ticket in tickets:
+    if ticket['id'] == ticket_id and ticket['stock'] > 0:
+      ticket['stock'] -= 1
+      return jsonify({"message": "搶票成功！剩餘：" + str(ticket['stock'])})
+  return jsonify({"message": "搶票失敗，可能已售完"}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
