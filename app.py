7d56b3cb9980de1e2b377952828a8aa73befc58c
Nana0208-jamal <11156045@ntub.edu.tw>
2025-05-12 01:54:05 +0800

    切換節目詳情測試

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request
 from config import Config
 from models import db
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
+# 初始化資料庫
+db.init_app(app)
+
 # 主頁
 @app.route('/')
 def index():
     return render_template('index.html')
+
+#測試
+@app.route('/show/test')
+def test_detail():
+    return "這是節目詳情測試頁"
 
 # 購票起始頁
 @app.route('/ticket/start')
@@ -25,5 +33,6 @@ def select_area():
 def select_type():
     return render_template('select_type.html')
 
+# 執行伺服器
 if __name__ == '__main__':
     app.run(debug=True)
