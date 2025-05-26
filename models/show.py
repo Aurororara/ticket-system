2b1bbd8c43bcbef73fcda78bca5850a125d227dc
Catherine Yang <catherineyang@CatherinedeMacBook-Air.local>
2025-05-26 23:38:10 +0800

    改了一卡車東西

--- models/show.py
+++ models/show.py
@@ -1,7 +1,7 @@
 from models.db import db
+from sqlalchemy import Date
+from flask_sqlalchemy import SQLAlchemy
 
-def __repr__(self):
-    return f'<Show {self.show_name}>'
 
 class Show(db.Model):
     __tablename__ = 'show'
@@ -9,7 +9,12 @@ class Show(db.Model):
     show_name = db.Column(db.String(255))
     show_desc = db.Column(db.Text)
     show_pic = db.Column(db.Text)
+    start_date = db.Column(Date)
+    end_date = db.Column(Date)
     host_id = db.Column(db.Integer, db.ForeignKey('host.host_id'))
     location_id = db.Column(db.Integer, db.ForeignKey('location.loc_id'))
     createdAt = db.Column(db.Date)
     updatedAt = db.Column(db.Date)
+
+    def __repr__(self):
+        return f'<Show {self.show_name}>'
